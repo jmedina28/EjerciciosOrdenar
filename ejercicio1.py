7e6e1886d5c6c992abcc0d1df433cd44bfd8cdc3
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-03 08:31:38 +0000

    progresamos

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,9 @@
 # Ordenación por inserción dicotómica:
+"""Tengo que hacer lo siguiente:
+- Respecto a la lista base crear otra que contenga los mismos elementos ordenador por dicotomia:
+  - Cojo el elemento del medio y lo meto en una lista.
+  - Avanzo cogiendo elementos hacia la izquierda de la lista.
+  - Cuando acabe cogiendo las de la izquierda voy cogiendo las restantes que son las que quedaban a la derecha.
+- Finalmente hay que reordenar la lista obtenida como la original.
+"""
 print("ejer1")
