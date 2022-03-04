9a34253063f3bfbd99448ef0d97c5dc5e8d52839
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-04 15:57:40 +0000

    ordeno en una carpeta

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 variable = int(input("Seleccione que ejercicio desea ejecutar(1-3): "))
 if variable == 1:
-    import ejercicio1
+    from ejercicios import ejercicio1
 elif variable == 2:
-    import ejercicio2
+    from ejercicios import ejercicio2
 elif variable == 3:
-    import ejercicio3
+    from ejercicios import ejercicio3
 else:
     print("Introduzca valores entre 1 y 3.")
