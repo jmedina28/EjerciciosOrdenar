541f03cd9772b03ac63d411f10534ec631ef347d
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-04 10:54:11 +0000

    parte inicio

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,22 +1,16 @@
 # Ordenación por inserción dicotómica:
-"""Tengo que hacer lo siguiente:
-- Respecto a la lista base crear otra que contenga los mismos elementos ordenador por dicotomia:
-  - Cojo el elemento del medio y lo meto en una lista.
-  - Avanzo cogiendo elementos hacia la izquierda de la lista.
-  - Cuando acabe cogiendo las de la izquierda voy cogiendo las restantes que son las que quedaban a la derecha.
-- Finalmente hay que reordenar la lista obtenida como la original.
-"""
 lista = ["1","2","3","4","5","6","7"]
+orden = len(lista)
 listadicotomia = []
-listadicotomia.append(lista.pop(len(lista)//2))
-print(lista)
+print("Tu lista inicial es la siguiente: " + str(lista))
 retroceso = 0
 def dicotomia(longitud,retroceso):
-  retroceso = retroceso + 1
-  print(retroceso)
-  print(listadicotomia)
-  listadicotomia.append(lista[((longitud//2)-retroceso)-1])
-if len(listadicotomia) < len(lista):
-  dicotomia(len(lista),0)
-
-dicotomia(len(lista),0)
+  if  len(listadicotomia) <= orden//2:
+    retroceso += 1  
+    listadicotomia.append(lista.pop(round(longitud / 2)-retroceso))
+    dicotomia(orden, retroceso)
+  elif len(listadicotomia) > orden // 2 and len(lista) > 0:
+    listadicotomia.append(lista.pop(0))
+    dicotomia(longitud, retroceso)
+dicotomia(orden,retroceso)
+print("Esta es la lista obtenida mediante la dicotomía: " + str(listadicotomia))
