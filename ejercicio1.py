aee154cec4b42e69481731b8b5c6060479de2dd1
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-04 14:24:13 +0000

    modificación

--- ejercicio1.py
+++ ejercicio1.py
@@ -17,11 +17,11 @@ listadicotomia = []
 print("Tu lista inicial ordenada es la siguiente: " + str(lista))
 retroceso = 0
 def dicotomia(longitud,retroceso):
-  if  len(listadicotomia) <= orden//2:
+  if  len(listadicotomia) < orden//2:
     retroceso += 1  
     listadicotomia.append(lista.pop(round(longitud / 2)-retroceso))
     dicotomia(orden, retroceso)
-  elif len(listadicotomia) > orden // 2 and len(lista) > 0:
+  elif len(listadicotomia) >= orden // 2 and len(lista) > 0:
     listadicotomia.append(lista.pop(0))
     dicotomia(longitud, retroceso)
 dicotomia(orden,retroceso)
