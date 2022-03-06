581371f2ab93a69320bd03f39eb0fc9e9f2d90d3
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-06 13:56:05 +0000

    avances

--- ejercicios/ejercicio3.py
+++ ejercicios/ejercicio3.py
@@ -2,12 +2,10 @@
 # Para la realización de este ejercicio voy a reutilizar una parte del código del primero.
 import random
 listainicial = []
-
 def generarlista(n, i):
     j = i.split("-")
     while len(listainicial) < n:
         listainicial.append(random.randint(int(j[0]), int(j[1])))
-
 generarlista(int(input("Introduzca cuántos elementos desea que tenga su lista: ")),
              str(input("""Introduzca el intervalo que desea que abarque la lista generada. 
 Para ello escríbalo tal y como se muestra en el ejemplo dado.
@@ -15,25 +13,24 @@ Para ello escríbalo tal y como se muestra en el ejemplo dado.
 - Introduzca su intervalo: """)))
 print("Tu lista inicial es la siguiente: " + str(listainicial))
 
-
-
+def diccionariolistas(j):
+    diccionario = {}
+    for i in range(1, j + 1):
+        diccionario[i] = []
+    return diccionario
+resultado = diccionariolistas(len(listainicial))
+print(resultado)
 # Este programa va a extraer segmentos decrecientes de la lista generada.
+indicemax = listainicial.index(max(listainicial))
+resultado[1].append(listainicial[indicemax])
+print(resultado[1])
 
-orden = len(listainicial)
-maximo = max(listainicial)
-indicemax = listainicial.index(maximo)
-segmento1= []
 def generarsegmento1(n, i):
-  segmento1.append(listainicial[n])
-  if listainicial[n+i]<listainicial[n] and ((n+len(segmento1)))<=len(listainicial):
-    segmento1.append(listainicial[n+1])
-    print(segmento1)
-    generarsegmento1(n+len(segmento1), 1)
-   
-  
-generarsegmento1(indicemax, 1)
-
-
-
-
+  if (n+i+1)<len(listainicial) and listainicial[n+i+1]<=listainicial[n+i]:
+    resultado[1+i].append(listainicial[indicemax+i+1])
+    generarsegmento1(n, i +1)
+  else:
+    print(resultado)
     
+
+generarsegmento1(indicemax, 0)
