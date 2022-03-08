8805f14667814ec0a7b2329d6dc94cff4d666183
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-08 09:03:29 +0000

    parece que va a funcionar

--- ejercicios/ejercicio3.py
+++ ejercicios/ejercicio3.py
@@ -1,19 +1,9 @@
 # Completar las especificaciones:
 # Para la realización de este ejercicio voy a reutilizar una parte del código del primero.
-import random
-listainicial = []
-def generarlista(n, i):
-    j = i.split("-")
-    while len(listainicial) < n:
-        listainicial.append(random.randint(int(j[0]), int(j[1])))
-generarlista(int(input("Introduzca cuántos elementos desea que tenga su lista: ")),
-             str(input("""Introduzca el intervalo que desea que abarque la lista generada. 
-Para ello escríbalo tal y como se muestra en el ejemplo dado.
-- Ejemplo: 0-100
-- Introduzca su intervalo: """)))
-print("Tu lista inicial es la siguiente: " + str(listainicial))
-listainicial =  [12, 96, 11, 53, 157, 118, 40, 198, 200, 113, 85, 125, 138, 109, 141, 102, 167, 128, 114, 168]
+
+listainicial =  [12, 11, 10, 53, 157, 118, 40, 198, 200, 113, 85, 125, 138, 109, 141, 102, 167, 128, 114, 168]
 print(listainicial)
+
 def diccionariolistas(j):
     diccionario = {}
     for i in range(1, j + 1):
@@ -22,24 +12,23 @@ def diccionariolistas(j):
 resultado = diccionariolistas(len(listainicial))
 print(resultado)
 # Este programa va a extraer segmentos decrecientes de la lista generada.
-indicemax = listainicial.index(max(listainicial))
-resultado[1].append(listainicial[indicemax])
-
-def generarsegmento(p,n, i):
-  while (n+i+1)<len(listainicial) and listainicial[n+i+1]<=listainicial[n+i] and p<=20:
-    resultado[p].append(listainicial[indicemax+i+1])
-    i += 1
+resultado[1].append(listainicial.pop(0))
+def generarsegmento(n,p):
+  if n<=20 and p<20 and resultado[n][p] >= listainicial[0]:
+    resultado[n].append(listainicial.pop(0))
     print(resultado)
-  else:
-    p = 2
-    while  p <= n:
-      resultado[p].append(listainicial[indicemax+i+1])
-      i += 1
-      p +=1 
-    
-    listainicial.index(max(listainicial))
-    
-  print(resultado)
+    print(n,p)
+    if n<20 and p<20 and len(listainicial)>0:
+      generarsegmento(n,p+1)
+    else:
+      print(resultado)
+  elif n<20 and p<=20 and resultado[n][p] < listainicial[0]:
+    resultado[n+1].append(listainicial.pop(0))
 
-    
-generarsegmento(1, indicemax, 0)
+    if n<20 and p<20 and len(listainicial)>0:
+      generarsegmento(n+1,0)
+    else:
+      print(resultado)
+
+  
+generarsegmento(1,0)
