81e527b6e8ec819b807a2a2c6b5356315146c22a
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-08 09:11:04 +0000

    parece que funciona

--- ejercicios/ejercicio3.py
+++ ejercicios/ejercicio3.py
@@ -1,8 +1,19 @@
 # Completar las especificaciones:
 # Para la realización de este ejercicio voy a reutilizar una parte del código del primero.
+import random
+listainicial = []
 
-listainicial =  [12, 11, 10, 53, 157, 118, 40, 198, 200, 113, 85, 125, 138, 109, 141, 102, 167, 128, 114, 168]
-print(listainicial)
+def generarlista(n, i):
+    j = i.split("-")
+    while len(listainicial) < n:
+        listainicial.append(random.randint(int(j[0]), int(j[1])))
+
+generarlista(int(input("Introduzca cuántos elementos desea que tenga su lista: ")),
+             str(input("""Introduzca el intervalo que desea que abarque la lista generada. 
+Para ello escríbalo tal y como se muestra en el ejemplo dado.
+- Ejemplo: 0-100
+- Introduzca su intervalo: """)))
+print("Tu lista inicial es la siguiente: " + str(listainicial))
 
 def diccionariolistas(j):
     diccionario = {}
@@ -10,25 +21,21 @@ def diccionariolistas(j):
         diccionario[i] = []
     return diccionario
 resultado = diccionariolistas(len(listainicial))
-print(resultado)
 # Este programa va a extraer segmentos decrecientes de la lista generada.
 resultado[1].append(listainicial.pop(0))
+
 def generarsegmento(n,p):
   if n<=20 and p<20 and resultado[n][p] >= listainicial[0]:
     resultado[n].append(listainicial.pop(0))
-    print(resultado)
-    print(n,p)
     if n<20 and p<20 and len(listainicial)>0:
       generarsegmento(n,p+1)
     else:
       print(resultado)
   elif n<20 and p<=20 and resultado[n][p] < listainicial[0]:
     resultado[n+1].append(listainicial.pop(0))
-
     if n<20 and p<20 and len(listainicial)>0:
       generarsegmento(n+1,0)
     else:
       print(resultado)
 
-  
 generarsegmento(1,0)
